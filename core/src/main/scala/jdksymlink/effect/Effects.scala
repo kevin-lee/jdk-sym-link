2cd310d6c1d8fa59949c3f20e4714986f534f38f
Kevin Lee <kevin.code@kevinlee.io>
2020-02-22 18:49:47 +1100

    Refactored

--- core/src/main/scala/jdksymlink/effect/Effects.scala
+++ core/src/main/scala/jdksymlink/effect/Effects.scala
@@ -1,7 +1,10 @@
 package jdksymlink.effect
 
-import cats.Id
-import cats.effect.IO
+import cats._
+import cats.implicits._
+import cats.effect._
+
+import jdksymlink.core.data.YesOrNo
 
 /**
  * @author Kevin Lee
@@ -37,7 +40,44 @@ object EffectConstructor {
 
 }
 
+trait ConsoleEffect[F[_]] {
+  def readLn: F[String]
+
+  def putStrLn(value: String): F[Unit]
+
+  def readYesOrNo(prompt: String): F[YesOrNo]
+}
+
+object ConsoleEffect {
+  def apply[F[_]: ConsoleEffect]: ConsoleEffect[F] = implicitly[ConsoleEffect[F]]
+
+  implicit val ioConsoleEffect: ConsoleEffect[IO] = new ConsoleEffectF[IO]
+
+  final class ConsoleEffectF[F[_] : EffectConstructor : Monad] extends ConsoleEffect[F] {
+    override def readLn: F[String] =
+      EffectConstructor[F].effect(scala.io.StdIn.readLine)
+
+    override def putStrLn(value: String): F[Unit] =
+      EffectConstructor[F].effect(println(value))
+
+    override def readYesOrNo(prompt: String): F[YesOrNo] = for {
+      _ <- putStrLn(prompt)
+      answer <- readLn
+      yesOrN <-  answer match {
+        case "y" | "Y" =>
+          EffectConstructor[F].effect(YesOrNo.yes)
+        case "n" | "N" =>
+          EffectConstructor[F].effect(YesOrNo.no)
+        case _ =>
+          readYesOrNo(prompt)
+      }
+    } yield yesOrN
+
+  }
+}
+
 trait Effectful[F[_]] {
+
   protected def EF: EffectConstructor[F]
 
   def effect[A](a: => A): F[A] = EF.effect(a)
@@ -45,4 +85,17 @@ trait Effectful[F[_]] {
   def pureEffect[A](a: A): F[A] = EF.pureEffect(a)
 
   def effectUnit: F[Unit] = EF.unit
+
+}
+
+trait ConsoleEffectful[F[_]] {
+
+  protected def CF: ConsoleEffect[F]
+
+  def readLn: F[String] = CF.readLn
+
+  def putStrLn(value: String): F[Unit] = CF.putStrLn(value)
+
+  def readYesOrNo(prompt: String): F[YesOrNo] = CF.readYesOrNo(prompt)
+
 }
