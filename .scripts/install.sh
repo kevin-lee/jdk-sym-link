6e4d9bdbed07a8524f8d8a2e44ebd5e0a17ee90c
Kevin Lee <kevin.code@kevinlee.io>
2021-02-26 00:41:16 +1100

    Added: How to install

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -5,7 +5,7 @@ set -eu
 app_original_executable_name=jdk-sym-link
 app_executable_name=jdkslink
 app_name=jdk-sym-link-cli
-app_version=${1:-0.2.0}
+app_version=${1:-0.2.1}
 versioned_app_name="${app_name}-${app_version}"
 app_zip_file="${versioned_app_name}.zip"
 download_url="https://github.com/Kevin-Lee/jdk-sym-link/releases/download/v${app_version}/${app_zip_file}"
